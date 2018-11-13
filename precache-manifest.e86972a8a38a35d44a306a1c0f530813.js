be93f2916bf89c2ae052714554e0a67747a0f090
Augus-top <acpluschkat@hotmail.com>
2018-11-13 20:42:03 -0200

    Updates

--- precache-manifest.e86972a8a38a35d44a306a1c0f530813.js
+++ precache-manifest.e86972a8a38a35d44a306a1c0f530813.js
@@ -8,8 +8,8 @@ self.__precacheManifest = [
     "url": "/wheel-of-fate/static/media/error.fd0a7a21.png"
   },
   {
-    "revision": "243a78c7098f708ed41cb5a47e0dc2c1",
-    "url": "/wheel-of-fate/static/media/cardblue.243a78c7.png"
+    "revision": "d5c52a45cffe935969b2218ae55b84f5",
+    "url": "/wheel-of-fate/static/media/cardblue.d5c52a45.png"
   },
   {
     "revision": "21ec86ea3852d7fecc910ae2e45f10c7",
@@ -20,15 +20,15 @@ self.__precacheManifest = [
     "url": "/wheel-of-fate/static/js/runtime~main.df52a23e.js"
   },
   {
-    "revision": "786863a45895ba286441",
-    "url": "/wheel-of-fate/static/js/main.786863a4.chunk.js"
+    "revision": "60c4ab44c0350b8545c6",
+    "url": "/wheel-of-fate/static/js/main.60c4ab44.chunk.js"
   },
   {
     "revision": "8a93a9ed990c448de98d",
     "url": "/wheel-of-fate/static/js/1.8a93a9ed.chunk.js"
   },
   {
-    "revision": "786863a45895ba286441",
+    "revision": "60c4ab44c0350b8545c6",
     "url": "/wheel-of-fate/static/css/main.d353dcdf.chunk.css"
   },
   {
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "/wheel-of-fate/static/css/1.50c358a6.chunk.css"
   },
   {
-    "revision": "cc33e5e769b0eb7b290057c4ba2774cf",
+    "revision": "41d060bdbe3f00a5652b2731e0b69993",
     "url": "/wheel-of-fate/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wheel-of-fate/precache-manifest.007aa8f14ee7d8e529ad8ad57c4f20df.js"
+  "/wheel-of-fate/precache-manifest.e86972a8a38a35d44a306a1c0f530813.js"
 );
 
 workbox.clientsClaim();

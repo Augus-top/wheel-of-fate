7549def960c58bce69c5703db8490c6171bba89b
Augus-top <acpluschkat@hotmail.com>
2018-11-13 20:22:44 -0200

    Updates

---
+++ precache-manifest.4a4a524b6c407fac424aeb66fb339447.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "023263a0467d6b5ed6377efeec080f2f",
+    "url": "/wheel-of-fate/static/media/git_icon.023263a0.svg"
+  },
+  {
+    "revision": "fd0a7a21835545ee6b4b146d38d5a651",
+    "url": "/wheel-of-fate/static/media/error.fd0a7a21.png"
+  },
+  {
+    "revision": "243a78c7098f708ed41cb5a47e0dc2c1",
+    "url": "/wheel-of-fate/static/media/cardblue.243a78c7.png"
+  },
+  {
+    "revision": "1bc16fd735e0b049d96b88196054ebd4",
+    "url": "/wheel-of-fate/static/media/back.1bc16fd7.png"
+  },
+  {
+    "revision": "df52a23ef9e652acfcf1",
+    "url": "/wheel-of-fate/static/js/runtime~main.df52a23e.js"
+  },
+  {
+    "revision": "7c69ae33ff16ba8a5f82",
+    "url": "/wheel-of-fate/static/js/main.7c69ae33.chunk.js"
+  },
+  {
+    "revision": "8a93a9ed990c448de98d",
+    "url": "/wheel-of-fate/static/js/1.8a93a9ed.chunk.js"
+  },
+  {
+    "revision": "7c69ae33ff16ba8a5f82",
+    "url": "/wheel-of-fate/static/css/main.41aa1cf0.chunk.css"
+  },
+  {
+    "revision": "8a93a9ed990c448de98d",
+    "url": "/wheel-of-fate/static/css/1.50c358a6.chunk.css"
+  },
+  {
+    "revision": "596bc41fe9a51a498cfcbd923c0803af",
+    "url": "/wheel-of-fate/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d3fb7746a575ecff3098f686522b2b9.js"
+  "/wheel-of-fate/precache-manifest.4a4a524b6c407fac424aeb66fb339447.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/wheel-of-fate/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

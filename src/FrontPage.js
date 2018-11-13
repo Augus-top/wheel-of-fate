63e932285c7be7ec4cf0e445db2be1dac70d62f8
Augus-top <acpluschkat@hotmail.com>
2018-11-12 23:35:53 -0200

    Rotation and Angle

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -3,14 +3,42 @@ import React, { Component } from 'react';
 // import { Search, Footer } from './common';
 import gitIcon from './images/git_icon.svg';
 import cardImage from './images/cardblue.png';
+import cardImage2 from './images/cardred.png';
 
 const frontHeaderMsg = 'The Wheel of Fate is Turning';
 
-class FrontCards extends Component {
+class FrontCard extends Component {
+
+  getImgAngle(img) {
+    const matrix = window.getComputedStyle(img, null).getPropertyValue('transform');
+    const values = matrix.split('(')[1].split(')')[0].split(',');
+    const a = values[0];
+    const b = values[1];
+    let angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+    if (angle < 0) angle = 360 + angle; 
+    return angle;
+  }
+
+  imageClick(e) {
+    console.log('Click');
+    const img = e.target;
+    const angle = this.getImgAngle(img);
+    console.log(angle);
+    // const image = e.target;
+    // image.classList.toggle('is-flipped');
+    // console.log(image);
+    // const front = document.querySelector('.cardFront');
+    // front.classList.toggle('is-flipped');
+    // const back = document.querySelector('.main-front');
+    // back.classList.toggle('is-flipped');
+    // console.log(card);
+  } 
+
   render() {
     return(
       <main className='main-front'>
-        <img src={cardImage} alt='Cards'/>
+        <img className='card cardBack' src={cardImage} onClick={(e) => this.imageClick(e)} alt='Card'/>
+        <img className='card cardFront' src={cardImage2} onClick={(e) => this.imageClick(e)} alt='Card'/>
       </main>
     );
   }
@@ -63,8 +91,8 @@ export default class FrontPage extends Component {
   render() {
     return (
       <div className='grid-wrapper'>
-        <FrontHeader headerMsg={frontHeaderMsg} typingSpeed='100'/>
-        <FrontCards/>
+        <FrontHeader headerMsg={frontHeaderMsg} typingSpeed='30'/>
+        <FrontCard/>
         <Footer/>
       </div>
     );
